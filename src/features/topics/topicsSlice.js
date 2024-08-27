b4a0b4bc743828d1e3ae29e9fe30335b06ec7712
Odiovock <64711277+Odiovock@users.noreply.github.com>
2024-08-26 20:58:20 -0400

    topicsSlice action

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -1,8 +1,30 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
+const initialState = {
+    topics: {}
+}
+
 export const topicsSlice = createSlice({
-    name: 'topicSlice',
-    initialState: {
-        topics: {}
+    name: 'topics',
+    initialState,
+    reducers: {
+        addTopic: (state, action) => {
+            const {id, name, icon} = action.payload;
+            
+            state.topics[id] = {
+                id: id,
+                name: name,
+                icon: icon,
+                quizIds: []
+            }
+        }
     }
 });
+
+export const addTopic = (topic) => {
+    
+
+    return {
+
+    }
+}
